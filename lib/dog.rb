bc38dbfaa145111840264081c37758abc04162ff
lewiih001 <lewismunyambu@gmail.com>
2022-11-02 17:42:21 +0300

    completed lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,4 +36,45 @@ class Dog
         end
         self
       end
+      def self.create(name:, breed:)
+        dog = Dog.new(name: name, breed: breed)
+        dog.save
+      end
+      def self.new_from_db(row)
+        self.new(id: row[0], name: row[1], breed: row[2])
+      end
+  
+      def self.all
+        sql = <<-SQL
+          SELECT *
+          FROM dogs;
+        SQL
+    
+        DB[:conn].execute(sql).map do |row|
+          self.new_from_db(row)
+        end
+      end
+      def self.find_by_name(name)
+        sql = <<-SQL
+            SELECT * FROM dogs
+            WHERE name = ?
+            LIMIT 1
+        SQL
+
+        DB[:conn].execute(sql, name).map do |row|
+            self.new_from_db(row)
+        end.first
+    end
+    def self.find(id)
+        sql = <<-SQL
+            SELECT * FROM dogs
+            WHERE id = ?
+            LIMIT 1
+        SQL
+
+        DB[:conn].execute(sql, id).map do |row|
+            self.new_from_db(row)
+        end.first
+
+    end
 end
